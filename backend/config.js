59849d04245f30a8283391904a0dee7f1f817d61
Heidi4 <erickkaloki28@gmail.com>
2024-03-16 12:28:04 +0300

    MERN project

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT = 5555;
+export const mongoURL = 'mongodb+srv:root:root@books-store-mern.' // copy the strin in mongodb
---
+++ backend/index.js
@@ -0,0 +1,52 @@
+import express from "express" // npm i express nodemon
+import { PORT, mongoURL } from "./config.js"
+import { Book } from "./models/bookModel.js";
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome to Mern tutoria');
+
+});
+
+app.post('/books', async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: 'Send all required fields: author or publishYear'
+      })
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+
+    return response.status(200).send(book);
+  } 
+  catch (error) {
+    console.log(error.message);
+    response.status(500).send({message: error.message})
+  }
+})
+
+
+mongose // npm i mongose
+  .connect(mongoURL)
+  .then(() = {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    })
+
+  })
+  .catch((error) => {
+    console.log(error);
+  })
